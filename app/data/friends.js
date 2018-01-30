3140dac390cffa30fd19e63c86e15892b4440676
Charlotte Driscoll <charlotte.d.driscoll@icloud.com>
2018-01-30 14:22:39 -0600

    added content

---
+++ app/data/friends.js
@@ -0,0 +1,149 @@
+var friends = [
+    {
+        name: "Peter Pan",
+        photo: "https://lumiere-a.akamaihd.net/v1/images/image_76e1e8c7.jpeg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Prince Eric",
+        photo: "https://img.buzzfeed.com/buzzfeed-static/static/2016-11/18/9/asset/buzzfeed-prod-fastlane02/sub-buzz-20366-1479478670-2.png?downsize=715:*&output-format=auto&output-quality=auto",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Prince Philip ",
+        photo: "",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Prince Charming",
+        photo: "",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Beast",
+        photo: "",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Milo Thatch",
+        photo: "",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Hercules",
+        photo: "",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Jim Hawkins",
+        photo: "",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Tarzan",
+        photo: "",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+
+];
+
+module.exports = friends;
